fc55fd685e23bff4be939589589be292154e59f7
jcvillaquira <jc.villaquira@uniandes.edu.co>
2022-01-11 18:08:07 -0500

    Update generate_diagonal.py
    Solve the problem without using any external packages such as math.

--- generate_diagonal.py
+++ generate_diagonal.py
@@ -1,9 +1,12 @@
-import math
-
 # Return an array containing the numbers in the n-th diagonal of Pascal's triangle, to the specified length.
 def generate_diagonal(n, l):
-    diagonal = []
-    for k in range(0, l):
-        diagonal.append( math.comb(n+k, k) )
+    # Define an l*n+1 array which contains ones in the first row and last column.
+    X = [[1 if i==0 or j==l-1 else 0 for j in range(0,l)] for i in range(0,n+1)]
 
-    return diagonal
+    # Update the values of X adding the two values above.
+    for i in range(1, n+1):
+        for j in reversed(range(0, l-1)):
+            X[i][j] = X[i-1][j] + X[i][j+1]
+
+    # Return the last row (reverted).
+    return X[-1][::-1]
